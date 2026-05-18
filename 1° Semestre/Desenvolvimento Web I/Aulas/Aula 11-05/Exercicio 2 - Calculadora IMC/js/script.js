04c220a50b1a31b3a92c5844b9ac6d993f9f2426
Thiago Rodrigues <thiagorodriguess2007@gmail.com>
2026-05-18 15:12:46 -0300

    style(exercicio-2): adiciona CSS e ajusta HTML e JS

--- 1° Semestre/Desenvolvimento Web I/Aulas/Aula 11-05/Exercicio 2 - Calculadora IMC/js/script.js
+++ 1° Semestre/Desenvolvimento Web I/Aulas/Aula 11-05/Exercicio 2 - Calculadora IMC/js/script.js
@@ -11,42 +11,48 @@ function classicacaoImc(IMC) {
     let classificacao = {
       nomeImc: "Abaixo do peso",
       textoImc: "Seu peso está abaixo do recomendado",
-      corImc: "#e49f1f",
+      corBordaImc: "#e49f1f",
+      corFundoImc: "#e49f1f20",
     };
     return classificacao;
   } else if (18.5 <= IMC && IMC < 25) {
     let classificacao = {
       nomeImc: "Peso normal",
       textoImc: "Parabéns! Seu peso está dentro do adequado.",
-      corImc: "#1f85e4",
+      corBordaImc: "#1f85e4",
+      corFundoImc: "#1f85e420",
     };
     return classificacao;
   } else if (25 <= IMC && IMC < 30) {
     let classificacao = {
       nomeImc: "Sobrepeso",
       textoImc: "Atenção! Seu peso está acima do recomendado.",
-      corImc: "#e4e11f",
+      corBordaImc: "#e4e11f",
+      corFundoImc: "#e4e11f20",
     };
     return classificacao;
   } else if (30 <= IMC && IMC < 35) {
     let classificacao = {
       nomeImc: "Obesidade Grau I",
       textoImc: "Procure acompanhamento médico e nutricional.",
-      corImc: "#e4b61f",
+      corBordaImc: "#e4b61f",
+      corFundoImc: "#e4b61f20",
     };
     return classificacao;
   } else if (35 <= IMC && IMC < 40) {
     let classificacao = {
       nomeImc: "Obesidade Grau II",
       textoImc: "Maior risco para saúde, recomenda-se avaliação médica.",
-      corImc: "#e46e1f",
+      corBordaImc: "#e46e1f",
+      corFundoImc: "#e46e1f20",
     };
     return classificacao;
   } else {
     let classificacao = {
       nomeImc: "Obesidade Grave/Grave III",
       textoImc: "Risco muito grave para saúde, acompanhamento urgente necessário.",
-      corImc: "#e4261f",
+      corBordaImc: "#e4261f",
+      corFundoImc: "#e4261f20",
     };
     return classificacao;
   }
@@ -71,9 +77,11 @@ document.getElementById("form-imc").addEventListener("submit", (event) => {
     resultado.textoImc = "Para crianças e adolescentes, é necessário usar curvas de crescimento da OMS. Procure um pediatra para avaliação adequada.";
   }
 
-  textoImc.innerHTML = `O seu IMC é ${IMC.toFixed(2)}`;
+  textoImc.innerHTML = `O seu IMC é: <strong>${IMC.toFixed(2)}</strong>`;
 
   document.getElementById('nome-imc').innerHTML = resultado.nomeImc;
   document.getElementById('texto-imc').innerHTML = resultado.textoImc;
-  document.getElementById('resultado').style.borderColor = resultado.corImc;
+  
+  document.getElementById('resultado').style.background = resultado.corFundoImc;
+  document.getElementById('resultado').style.borderColor = resultado.corBordaImc;
 });
