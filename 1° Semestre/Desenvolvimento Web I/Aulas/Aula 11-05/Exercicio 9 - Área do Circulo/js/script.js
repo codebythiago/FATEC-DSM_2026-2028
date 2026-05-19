dc5391f3c889e44ea03024f31e588f6130ba4300
Thiago Rodrigues <thiagorodriguess2007@gmail.com>
2026-05-19 17:18:18 -0300

    style(1-semestre/desenvolvimento-web-1/aulas/aula 11-05/exercicio-9): adicionando estilização CSS e ajustando regras no JS

--- 1° Semestre/Desenvolvimento Web I/Aulas/Aula 11-05/Exercicio 9 - Área do Circulo/js/script.js
+++ 1° Semestre/Desenvolvimento Web I/Aulas/Aula 11-05/Exercicio 9 - Área do Circulo/js/script.js
@@ -1,4 +1,3 @@
-
 /* Primeiramente, qual o fluxo?
   
 1. O usuário digita os dois valores e escolhe o tipo de unidade da base, altura e área do triângulo.
@@ -15,42 +14,46 @@ const unidades = {
   m: 1,
   dam: 10,
   hm: 100,
-  km: 1000
-}
+  km: 1000,
+};
 
 // 3. Ambas as unidades são convertidas para metros (base e altura)
 function converterParaMetros(unidade, valor) {
-  return valor * unidades[unidade]
+  return valor * unidades[unidade];
 }
 
 function converterAreaFinal(unidade, areaEmMetros2) {
-  return areaEmMetros2 / Math.pow(unidades[unidade], 2)
+  return areaEmMetros2 / Math.pow(unidades[unidade], 2);
 }
 
-const calcularBtn = document.getElementById('calcular-btn');
-const resultadoUnidadeArea = document.getElementById('res-unidade-area');
+const calcularBtn = document.getElementById("calcular-btn");
+const resultadoUnidadeArea = document.getElementById("res-unidade-area");
 
 function calcularAreaCirculo(raio) {
-  return Math.pow(raio, 2);
+  return raio * raio;
 }
 
-calcularBtn.addEventListener('click', function () {
-  const raio = Number(document.getElementById('raio').value) || 0;
-  const tipoUnidadeRaio = document.getElementById('unidade-raio').value;
-  const tipoUnidadeArea = document.getElementById('unidade-area').value;
+calcularBtn.addEventListener("click", function () {
+  const raio = Number(document.getElementById("raio").value) || 0;
+  const tipoUnidadeRaio = document.getElementById("unidade-raio").value;
+  const tipoUnidadeArea = document.getElementById("unidade-area").value;
 
   // Converter para metros
   const raioMetros = converterParaMetros(tipoUnidadeRaio, raio);
-  const areaEmMetros2 = calcularAreaCirculo(raioMetros)
+  const areaEmMetros2 = calcularAreaCirculo(raioMetros);
 
   // Converter para unidade de área escolhida
   const areaFinal = converterAreaFinal(tipoUnidadeArea, areaEmMetros2);
 
-  resultadoUnidadeArea.innerHTML = `${areaFinal}&pi; ${tipoUnidadeArea}<sup>2</sup> ou ${(areaFinal * 3.1415)} ${tipoUnidadeArea}<sup>2</sup>`;
-})
+  if (areaFinal === 0) {
+    resultadoUnidadeArea.innerHTML = `0.00 ${tipoUnidadeArea}<sup>2</sup>`;
+  } else {
+    resultadoUnidadeArea.innerHTML = `${areaFinal.toFixed(2)}&pi; ${tipoUnidadeArea}<sup>2</sup> ou &approx; ${(areaFinal * 3.1415).toFixed(2)} ${tipoUnidadeArea}<sup>2</sup>`;
+  }
+});
 
-document.addEventListener('keydown', (event) => {
-  if(event.key === 'Enter') {
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
     calcularBtn.click();
   }
 });
