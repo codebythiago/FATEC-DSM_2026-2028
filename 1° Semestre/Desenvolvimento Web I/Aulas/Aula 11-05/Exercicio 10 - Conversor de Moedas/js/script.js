99ef77aa7a2f9847cb7233a2ef4be1ad29522a00
Thiago Rodrigues <thiagorodriguess2007@gmail.com>
2026-05-20 00:27:31 -0300

    feat(1-semestre/desenvolvimento-web-1/aulas/aula 11-05/exercicio 6 e 10): adicionado script.js para o exercício 6 e ajustado script.js do exercício 10 para ser conta com valores fixos.

--- 1° Semestre/Desenvolvimento Web I/Aulas/Aula 11-05/Exercicio 10 - Conversor de Moedas/js/script.js
+++ 1° Semestre/Desenvolvimento Web I/Aulas/Aula 11-05/Exercicio 10 - Conversor de Moedas/js/script.js
@@ -1,36 +1,42 @@
-async function buscarTaxaConversao(moedaDe, moedaPara) {
-    const url = `https://v6.exchangerate-api.com/v6/${EXCHANT_RATE_API_KEY}/pair/${moedaDe}/${moedaPara}`;
-    
-    const resposta = await fetch(url);
-    const dados = await resposta.json();
+// taxas fixas em relação ao REAL (BRL)
+const taxas = {
+    BRL: 1,
+    USD: 5.06,
+    EUR: 5.87,
+    GBP: 6.78,
+    JPY: 0.032,
+    CNY: 0.74
+};
 
-    return dados.conversion_rate;
+function buscarTaxaConversao(moedaDe, moedaPara) {
+    const taxaBase = taxas[moedaDe];
+    const taxaDestino = taxas[moedaPara];
+
+    return taxaBase / taxaDestino;
 }
 
 function simboloMoeda(moeda) {
-    if(moeda === 'BRL') return "R&#36;";
-    if(moeda === 'USD') return "&#36;";
-    if(moeda === 'EUR') return "&euro;";
-    if(moeda === 'GBP') return "&pound;";
-    if(moeda === 'JPY') return "&yen;";
-    if(moeda === 'CNY') return "&#20803;";
+    if (moeda === 'BRL') return "R$";
+    if (moeda === 'USD') return "$";
+    if (moeda === 'EUR') return "€";
+    if (moeda === 'GBP') return "£";
+    if (moeda === 'JPY') return "¥";
+    if (moeda === 'CNY') return "元";
 }
 
-document.getElementById('form-moedas').addEventListener('submit', async (event) => {
+document.getElementById('form-moedas').addEventListener('submit', (event) => {
     event.preventDefault();
 
     const valor = Number(document.getElementById('valor').value);
     const moedaDe = document.getElementById('moedaDe').value;
     const moedaPara = document.getElementById('moedaPara').value;
+
     const simboloMoedaDe = simboloMoeda(moedaDe);
     const simboloMoedaPara = simboloMoeda(moedaPara);
 
-    let taxa = await buscarTaxaConversao(moedaDe, moedaPara);
-    let conversao = valor * taxa;
+    const taxa = buscarTaxaConversao(moedaDe, moedaPara);
+    const conversao = valor * taxa;
 
-    document.getElementById('resultado').innerHTML = `${simboloMoedaDe} ${valor.toFixed(2)} equivale à ${simboloMoedaPara} ${conversao.toFixed(2)}`;    
-})
-
-document.addEventListener('keydown', (event) => {
-    document.getElementById('converter-btn').click();
-})
+    document.getElementById('resultado').innerHTML =
+        `<p><b>${simboloMoedaDe} ${valor.toFixed(2)}</b> equivale à <b>${simboloMoedaPara} ${conversao.toFixed(2)}</b></p>`;
+});
