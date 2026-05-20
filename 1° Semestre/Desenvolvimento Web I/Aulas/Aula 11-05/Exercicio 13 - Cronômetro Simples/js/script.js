4f4ab8d1523e45f626a26aadf37948cd02a91308
Thiago Rodrigues <thiagorodriguess2007@gmail.com>
2026-05-19 22:09:40 -0300

    feat(1-semestre/desenvolvimento-web-1/aulas/aula 11-05/exercicio-13): adicionado script de voltas e melhorando funcionamento do script.

--- 1° Semestre/Desenvolvimento Web I/Aulas/Aula 11-05/Exercicio 13 - Cronômetro Simples/js/script.js
+++ 1° Semestre/Desenvolvimento Web I/Aulas/Aula 11-05/Exercicio 13 - Cronômetro Simples/js/script.js
@@ -1,88 +1,143 @@
-const displayCronometro = document.getElementById("display-cronometro");
+const tempoPrincipal = document.getElementById("tempo-principal");
+const tempoVolta = document.getElementById("tempo-volta");
 let horas = 0;
 let minutos = 0;
 let segundos = 0;
 let estaRodando = false;
 let intervalo;
 
+let numeroVoltas = 0;
+let horasVolta = 0;
+let minutosVolta = 0;
+let segundosVolta = 0;
+let intervaloVolta = 0;
+
+function formatar(valor) {
+  if (valor < 10) {
+    return `0${valor}`;
+  }
+  return valor;
+}
+
+function cronometroFormatado(horas, minutos, segundos) {
+  return `${formatar(horas)}:${formatar(minutos)}:${formatar(segundos)}`;
+}
+
 function atualizarDisplay() {
-  displayCronometro.innerHTML = `${horas < 10 ? 0 : ""}${horas}:${minutos < 10 ? 0 : ""}${minutos}:${segundos < 10 ? 0 : ""}${segundos}`;
+  let tempoAtual = cronometroFormatado(horas, minutos, segundos);
+  tempoPrincipal.innerHTML = tempoAtual;
+
+  tempoVolta.innerHTML = cronometroFormatado(
+    horasVolta,
+    minutosVolta,
+    segundosVolta,
+  );
+
+  if (tempoAtual !== "00:00:00") {
+    restaurarVoltaBtn.disabled = false;
+  } else {
+    restaurarVoltaBtn.disabled = true;
+  }
+
+  if (numeroVoltas > 0) {
+    tempoVolta.classList.add("ativo");
+  } else {
+    tempoVolta.classList.remove("ativo");
+  }
+}
+
+function calcularTempoPrincipal() {
+  segundos++;
+  if (segundos === 60) {
+    minutos++;
+    segundos = 0;
+  }
+
+  if (minutos === 60) {
+    horas++;
+    minutos = 0;
+  }
+  atualizarDisplay();
+}
+
+function calcularTempoVolta() {
+  segundosVolta++;
+  if (segundosVolta === 60) {
+    minutosVolta++;
+    segundosVolta = 0;
+  }
+
+  if (minutosVolta === 60) {
+    horasVolta++;
+    minutosVolta = 0;
+  }
+  atualizarDisplay();
 }
 
 function iniciar() {
-  if (!estaRodando) {
-    estaRodando = true;
-    intervalo = setInterval(function () {
-      segundos++;
-      if (segundos === 60) {
-        minutos++;
-        segundos = 0;
-      }
-      if (minutos === 60) {
-        horas++;
-        minutos = 0;
-      }
-      atualizarDisplay();
-    }, 1000);
-  }
+  estaRodando = true;
+  intervalo = setInterval(calcularTempoPrincipal, 1000);
+  intervaloVolta = setInterval(calcularTempoVolta, 1000);
 }
 
 function pausar() {
   clearInterval(intervalo);
+  clearInterval(intervaloVolta);
   estaRodando = false;
 }
 
 function restaurar() {
-  if (!estaRodando) {
-    horas = 0;
-    minutos = 0;
-    segundos = 0;
-    atualizarDisplay();
+  horas = minutos = segundos = 0;
+  numeroVoltas = 0;
+  atualizarDisplay();
+  document.querySelector("#voltas tbody");
+  restaurarVoltaBtn.innerText = "Volta";
+  playBtn.innerText = "Iniciar";
+  document.querySelector("#voltas tbody").innerHTML = "";
+}
+
+function adicionaVolta() {
+  numeroVoltas++;
+
+  const novaVolta = `<tr>
+    <td>${numeroVoltas}</td>
+    <td>${cronometroFormatado(horasVolta, minutosVolta, segundosVolta)}</td>
+    <td>${cronometroFormatado(horas, minutos, segundos)}</td>
+  </tr>`;
+  document.querySelector("#voltas tbody").innerHTML += novaVolta;
+
+  if (numeroVoltas > 0) {
+    tempoVolta.classList.add("ativo");
+  } else {
+    tempoVolta.classList.remove("ativo");
   }
+
+  tempoVolta.innerText = "00:00:00";
+  horasVolta = minutosVolta = segundosVolta = 0;
 }
 
 const playBtn = document.getElementById("play-btn");
-const restaurarBtn = document.getElementById("restaurar-volta-btn");
+const restaurarVoltaBtn = document.getElementById("restaurar-volta-btn");
 
 playBtn.addEventListener("click", () => {
-  if (estaRodando) {
-    pausar();
-    displayCronometro.innerText === "00:00:00"
-      ? (playBtn.innerHTML = "Iniciar")
-      : (playBtn.innerHTML = "Retomar");
-    if (
-      document.getElementById("display-cronometro").innerHTML !== "00:00:00" &&
-      !estaRodando
-    ) {
-      restaurarBtn.disabled = false;
-      restaurarBtn.innerHTML = "Restaurar";
-    }
-  } else {
+  if (!estaRodando) {
     iniciar();
-    playBtn.innerHTML = "Pausar";
-    if (
-      document.getElementById("display-cronometro").innerHTML !== "00:00:00" &&
-      estaRodando
-    ) {
-      restaurarBtn.innerHTML = "Volta";
-    }
+    playBtn.innerText = "Pausar";
+    restaurarVoltaBtn.innerText = "Volta";
+  } else if (estaRodando && tempoPrincipal.innerText === "00:00:00") {
+    pausar();
+    playBtn.innerText = "Iniciar";
+  } else {
+    pausar();
+    playBtn.innerText = "Retomar";
+    restaurarVoltaBtn.innerText = "Restaurar";
   }
 });
 
-displayCronometro.addEventListener('change', () => {
-  if(displayCronometro.innerHTML === "00:00:00") {
-    restaurarBtn.disabled = true;
-  } else {
-    restaurarBtn.disabled = false;
-  }
-})
-
-restaurarBtn.addEventListener('click', () => {
+restaurarVoltaBtn.addEventListener("click", () => {
   if (estaRodando) {
-    // Script de adicionar volta
+    adicionaVolta();
   } else {
     restaurar();
-    restaurarBtn.disabled = true;
-    restaurarBtn.innerHTML = 'Volta';
   }
 });
